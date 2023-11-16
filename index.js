16cf6e9aae24a9d6f6dca03440d17d2ed02b1951
Decady <36299716+Decady9@users.noreply.github.com>
2023-11-16 15:22:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { VK, Keyboard } from "vk-io";
 import { MongoClient } from "mongodb";
 import fs from 'fs';
-import { count } from "console";
-import { send } from "process";
+
+
+// Переменные
+let weightMin=0.123
 var a = 1;
 let now = new Date();
 const cfg = JSON.parse(fs.readFileSync('./config.json'));
@@ -11,49 +13,121 @@ const vk = new VK({
 });
 const client = new MongoClient('mongodb://127.0.0.1:27017');
 const db = client.db('Test').collection('Decady');
-
-
+//МАССИВЫ_____________________________________________________________________________________________
+const fish = ['Карп-кои', 'Щука', 'Окунь', 'Карась', 'Порванный сапог', 'Абоба', 'Кувшинка', 'Спрут', 'Рыба Фугу', 'Малёк'];
+const weightMax = [0.500, 1.000, 2.000, 3.000, 7.200, 10.000]
+//______________________________________________________НАЧАЛО___________________________________________
+//стартовая отладка
 vk.updates.start().then(() => {
-    console.log("Бот Запущен " + now);
+  console.log("Бот Запущен " + now);
+});
+//БАЗОВЫЕ ОТВЕТЫ НА СООБЩЕНИЯ
+vk.updates.hear('.продать всю рыбу', (context) => {
+context.send("В разработке....")
+
+});
+vk.updates.hear('.другое', (context) => {
+context.send("В разработке....")
 });
 
-  vk.updates.hear('рыбалка', (context) => {
-    
 
+  
+
+
+
+
+  vk.updates.hear('.рыбалка', (context) => {
     if(a==1){
+    console.log("Пользователь начал рыбалку " + now)
     context.send(`Вы начали рыбалку`);
     a = 0
-
     setTimeout(() => {
-   
+
       const fish = getFish();
-  
-      context.send(`Вы поймали ${fish}!`);
+      const weightMax = getMass();
+      let weight1 = Math.random()*(weightMax - weightMin) + weightMin;
+      weight1 = Math.round(weight1*1000)/1000;
+      if (fish == 'Порванный сапог'){
+        weight1 = 0.100;
+      }
+      const resultFirstSumm = sellets(weight1, fish);
+      //resultFirstSumm = "ПЕРВАЯ ПЕРЕМЕННАЯ В МОНГОДБ"
+      context.send(`Ваш улов:\n${fish} весом ${weight1} кг.\n\n\n\n\n\n\nЕго цена ${resultFirstSumm} монет`, {});;
       a = 1
     }, 9000)
-
     }
     else{
       context.send(`Вы должны дождаться предыдущей поклевки`);
     }
-    
-    
-  
-
-  });
+    });
 
 
-// Функция для получения случайной рыбы
+//________________Функции_____________________________________________________________________________
+//ШАНСЫ ВЫПАДЕНИЯ РЫБЫ
 function getFish() {
-  const fishes = ['окунь', 'карп', 'судак', 'щука', 'лосось', 'Порванный сапог'];
-  const randomIndex = Math.floor(Math.random() * fishes.length);
-  return fishes[randomIndex];
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+if (randomNumber <= 5) {
+return fish[0]; // Выполняем с шансом 5%
+} 
+else if (randomNumber <= 15) {
+return fish[1]; // Выполняем с шансом 10%
 }
+else if (randomNumber <= 50) {
+return fish[2]; // Выполняем с шансом 35%
+}
+else if (randomNumber <= 65) {  
+  return fish[3]; // Выполняем с шансом 15%
+  }
+ else {
+return fish[4]; // Не выполняем никакого действия с шансом 35%
+}
+}
+
+
+
+//ШАНСЫ ВЫПАДЕНИЯ МАССЫ РЫБЫ
+function getMass() {
+  const randomNumberTwo = Math.floor(Math.random() * 100) + 1;
+  if (randomNumberTwo <= 1) {
+  return weightMax[5];// Выполняем с шансом 1% максимальная масса 10кг
+  } 
+  else if (randomNumberTwo <= 11) {
+  return weightMax[3];// Выполняем с шансом 10% максимальная масса 3 кг
+  }
+  else if (randomNumberTwo <= 26) {
+  return weightMax[2]; // Выполняем с шансом 15% максимальная масса 2кг
+  }
+  else if (randomNumberTwo <= 31) {  
+    return weightMax[4]; // Выполняем с шансом 5% максимальная масска 7.2кг
+    }
+   else {
+  return weightMax[0]; //Выполняем с шансом 69% максимальная масса 0.5кг
+  }
+  }
+//СЧИТАЕТ ЦЕНУ ВЫЛОВЛЕННОЙ РЫБЫ
+function sellets(weight1, fish){
+  let coefficient
+  if (fish == 'Порванный сапог'){
+    coefficient = 0.0
+    // вот бы сюда переменную для каждого челове
+  }
+  else if (fish == 'Карп-кои'){
+    coefficient = 3.0
+  }
+  else if (fish == 'Окунь'){
+    coefficient = 0.4
+  }
+  else if (fish == 'Щука'){
+    coefficient = 1.0
+  }
+  else if (fish == 'Карась'){
+    coefficient = 0.55
+  }
+
+  let firstSumm = Math.round(weight1 * coefficient * 1000);
+  return firstSumm
+}
+
 
 // Авторизация и запуск бота
 vk.updates.start().catch(console.error); 
-
-
-function sellers(){
-
-}
